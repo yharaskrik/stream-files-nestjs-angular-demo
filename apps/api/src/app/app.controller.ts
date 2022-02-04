7305e0468792963a6cb4f09ca623701d85b0bd8b
jaybell <jaycooperbell@gmail.com>
2022-02-04 11:59:12 -0800

    init

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,15 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 
-import { Message } from '@stream-files-nestjs-demo/api-interfaces';
-
-import { AppService } from './app.service';
+import { Readable } from 'stream';
+import { Transform as Json2CsvTransform } from 'json2csv';
+import { randEmail, randFirstName, randUuid } from '@ngneat/falso';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get()
+  getData(@Res() res) {
+    // Built in Nodejs readable stream, this is our pass through that will stream one from to another
+    const input = new Readable({ objectMode: true });
+    input._read = () => {};
 
-  @Get('hello')
-  getData(): Message {
-    return this.appService.getData();
+    // Setting up json2csv for our output csv file
+    const fields = ['id', 'firstName', 'email'];
+    const opts = { fields };
+    const json2csvTransform = new Json2CsvTransform(opts, { objectMode: true });
+
+    // Need to make sure our response has the right headers so the browser can download the file
+    res.set({
+      'Content-Type': 'text/csv',
+      'Content-Disposition': 'attachment; filename=data.csv',
+    });
+
+    // We are piping our `input` (which is our passthrough) into the json2csv transformer and then into the response
+    input.pipe(json2csvTransform).pipe(res);
+
+    for (let i = 0; i < 100; i++) {
+      // Add data to our input stream which will then go through the json2csv transformer stream then into the response
+      input.push({
+        id: randUuid(),
+        firstName: randFirstName(),
+        email: randEmail(),
+      });
+    }
+
+    // end the stream
+    input.push(null);
   }
 }
